cf6ac99c90f10e5d115d3df1623f4dab76646af9
ardannetwork <ardannetwork@gmail.com>
2026-08-06 14:08:07 +0700

    layout A5

--- src/services/docxtemplater.ts
+++ src/services/docxtemplater.ts
@@ -4,108 +4,108 @@ import { saveAs } from 'file-saver';
 import { DisposisiSurat } from '../types/disposisi';
 import { jsPDF } from 'jspdf';
 
-// Generate raw docx XML content - Single full page layout matching PDF reference exactly
+// Generate raw docx XML content - Single unified table layout for A5
 const createDocxXmlTemplate = () => {
   return `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <w:document xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships">
   <w:body>
-    <!-- KOTAK JUDUL LEMBAR DISPOSISI -->
+    <!-- HEADER LEMBAR DISPOSISI -->
     <w:tbl>
       <w:tblPr>
         <w:tblW w:w="5000" w:type="pct"/>
         <w:tblBorders>
-          <w:top w:val="single" w:sz="12" w:space="0" w:color="000000"/>
-          <w:left w:val="single" w:sz="12" w:space="0" w:color="000000"/>
-          <w:bottom w:val="single" w:sz="12" w:space="0" w:color="000000"/>
-          <w:right w:val="single" w:sz="12" w:space="0" w:color="000000"/>
+          <w:top w:val="single" w:sz="10" w:space="0" w:color="000000"/>
+          <w:left w:val="single" w:sz="10" w:space="0" w:color="000000"/>
+          <w:bottom w:val="single" w:sz="10" w:space="0" w:color="000000"/>
+          <w:right w:val="single" w:sz="10" w:space="0" w:color="000000"/>
         </w:tblBorders>
       </w:tblPr>
       <w:tr>
         <w:tc>
           <w:p>
-            <w:pPr><w:jc w:val="center"/></w:pPr>
-            <w:r><w:rPr><w:b/><w:sz w:val="28"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>L E M B A R   D I S P O S I S I</w:t></w:r>
+            <w:pPr>
+              <w:jc w:val="center"/>
+              <w:spacing w:before="20" w:after="20"/>
+            </w:pPr>
+            <w:r><w:rPr><w:b/><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>L E M B A R   D I S P O S I S I</w:t></w:r>
           </w:p>
         </w:tc>
       </w:tr>
     </w:tbl>
 
-    <w:p><w:pPr><w:spacing w:after="160"/></w:pPr></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="40"/></w:pPr></w:p>
 
     <!-- METADATA SURAT -->
-    <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Surat Dari         : {surat_dari}</w:t></w:r></w:p>
-    <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>No. Surat         : {nomor_surat}</w:t></w:r></w:p>
-    <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Tgl. Surat         : {tanggal_surat}</w:t></w:r></w:p>
-    <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Diterima Tgl    : {diterima_tanggal}</w:t></w:r></w:p>
-    <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>No. Agenda     : {nomor_agenda}</w:t></w:r></w:p>
-    <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Sifat                 : {sifat}</w:t></w:r></w:p>
-    <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Hal                   : {hal}</w:t></w:r></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="20"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Surat Dari         : {surat_dari}</w:t></w:r></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="20"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>No. Surat         : {nomor_surat}</w:t></w:r></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="20"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Tgl. Surat         : {tanggal_surat}</w:t></w:r></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="20"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Diterima Tgl    : {diterima_tanggal}</w:t></w:r></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="20"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>No. Agenda     : {nomor_agenda}</w:t></w:r></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="20"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Sifat                 : {sifat}</w:t></w:r></w:p>
+    <w:p><w:pPr><w:spacing w:before="0" w:after="40"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Hal                   : {hal}</w:t></w:r></w:p>
 
-    <w:p><w:pPr><w:spacing w:after="160"/></w:pPr></w:p>
-
-    <!-- TABEL 2 KOLOM DISPOSISI -->
+    <!-- TABEL DISPOSISI DAN CATATAN DALAM 1 TABEL UTUH -->
     <w:tbl>
       <w:tblPr>
         <w:tblW w:w="5000" w:type="pct"/>
         <w:tblBorders>
-          <w:top w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:left w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:bottom w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:right w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:insideH w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:insideV w:val="single" w:sz="8" w:space="0" w:color="000000"/>
+          <w:top w:val="single" w:sz="6" w:space="0" w:color="000000"/>
+          <w:left w:val="single" w:sz="6" w:space="0" w:color="000000"/>
+          <w:bottom w:val="single" w:sz="6" w:space="0" w:color="000000"/>
+          <w:right w:val="single" w:sz="6" w:space="0" w:color="000000"/>
+          <w:insideH w:val="single" w:sz="6" w:space="0" w:color="000000"/>
+          <w:insideV w:val="single" w:sz="6" w:space="0" w:color="000000"/>
         </w:tblBorders>
       </w:tblPr>
+
       <!-- HEADER TABEL -->
       <w:tr>
         <w:tc>
           <w:tcPr><w:tcW w:w="2500" w:type="pct"/></w:tcPr>
-          <w:p><w:r><w:rPr><w:b/><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Diteruskan kepada Sdr :</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:b/><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Diteruskan kepada Sdr :</w:t></w:r></w:p>
         </w:tc>
         <w:tc>
           <w:tcPr><w:tcW w:w="2500" w:type="pct"/></w:tcPr>
-          <w:p><w:r><w:rPr><w:b/><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Dengan hormat harap :</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:b/><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Dengan hormat harap :</w:t></w:r></w:p>
         </w:tc>
       </w:tr>
-      <!-- CONTENT TABEL -->
+
+      <!-- CONTENT PETUGAS DAN INSTRUKSI -->
       <w:tr>
         <w:tc>
           <w:tcPr><w:tcW w:w="2500" w:type="pct"/></w:tcPr>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>1. {petugas_1}</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>2. {petugas_2}</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>3. {petugas_3}</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>4. {petugas_4}</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>1. {petugas_1}</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>2. {petugas_2}</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>3. {petugas_3}</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>4. {petugas_4}</w:t></w:r></w:p>
         </w:tc>
         <w:tc>
           <w:tcPr><w:tcW w:w="2500" w:type="pct"/></w:tcPr>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_tanggapan} Tanggapan dan saran</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_periksa} Periksa dan Proses lebih lanjut</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_koordinasi} Koordinasi/Konfirmasikan</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_arsipkan} Arsipkan</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_lain} {catatan_lain_text}</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_tanggapan} Tanggapan dan saran</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_periksa} Periksa dan Proses lebih lanjut</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_koordinasi} Koordinasi/Konfirmasikan</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_arsipkan} Arsipkan</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{chk_lain} {catatan_lain_text}</w:t></w:r></w:p>
         </w:tc>
       </w:tr>
-    </w:tbl>
-    <!-- KOTAK CATATAN -->
-    <w:tbl>
-      <w:tblPr>
-        <w:tblW w:w="5000" w:type="pct"/>
-        <w:tblBorders>
-          <w:top w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:left w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:bottom w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-          <w:right w:val="single" w:sz="8" w:space="0" w:color="000000"/>
-        </w:tblBorders>
-      </w:tblPr>
+
+      <!-- BARIS CATATAN (Gunakan gridSpan="2" agar gabung 2 kolom & langsung menempel) -->
       <w:tr>
         <w:tc>
-          <w:tcPr><w:tcW w:w="5000" w:type="pct"/></w:tcPr>
-          <w:p><w:r><w:rPr><w:b/><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Catatan :</w:t></w:r></w:p>
-          <w:p><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{catatan_text}</w:t></w:r></w:p>
+          <w:tcPr>
+            <w:tcW w:w="5000" w:type="pct"/>
+            <w:gridSpan w:val="2"/>
+          </w:tcPr>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:b/><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Catatan :</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="10" w:after="10"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{catatan_text}</w:t></w:r></w:p>
+          <w:p></w:p>
+          <w:p></w:p>
         </w:tc>
       </w:tr>
     </w:tbl>
-    <w:p><w:pPr><w:spacing w:after="280"/></w:pPr></w:p>
+
+    <w:p><w:pPr><w:spacing w:before="0" w:after="40"/></w:pPr></w:p>
+
     <!-- TANDA TANGAN KOORDINATOR -->
     <w:tbl>
       <w:tblPr>
@@ -116,18 +116,24 @@ const createDocxXmlTemplate = () => {
         </w:tblBorders>
       </w:tblPr>
       <w:tr>
-        <w:tc><w:tcPr><w:tcW w:w="2500" w:type="pct"/></w:tcPr><w:p/></w:tc>
+        <w:tc><w:tcPr><w:tcW w:w="2500" w:type="pct"/></w:tcPr><w:p><w:pPr><w:spacing w:before="0" w:after="0"/></w:pPr></w:p></w:tc>
         <w:tc>
           <w:tcPr><w:tcW w:w="2500" w:type="pct"/></w:tcPr>
-          <w:p><w:pPr><w:jc w:val="center"/></w:pPr><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Koordinator</w:t></w:r></w:p>
-          <w:p><w:pPr><w:jc w:val="center"/></w:pPr><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Wilayah Kerja IV Malang</w:t></w:r></w:p>
-          <w:p/>
-          <w:p/>
-          <w:p><w:pPr><w:jc w:val="center"/></w:pPr><w:r><w:rPr><w:b/><w:u w:val="single"/><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{disposisi_oleh}</w:t></w:r></w:p>
-          <w:p><w:pPr><w:jc w:val="center"/></w:pPr><w:r><w:rPr><w:sz w:val="22"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{nip_oleh}</w:t></w:r></w:p>
+          <w:p><w:pPr><w:jc w:val="center"/><w:spacing w:before="0" w:after="0"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Koordinator</w:t></w:r></w:p>
+          <w:p><w:pPr><w:jc w:val="center"/><w:spacing w:before="0" w:after="0"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>Wilayah Kerja IV Malang</w:t></w:r></w:p>
+          <w:p><w:pPr><w:spacing w:before="200" w:after="0"/></w:pPr></w:p>
+          <w:p><w:pPr><w:jc w:val="center"/><w:spacing w:before="0" w:after="0"/></w:pPr><w:r><w:rPr><w:b/><w:u w:val="single"/><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{disposisi_oleh}</w:t></w:r></w:p>
+          <w:p><w:pPr><w:jc w:val="center"/><w:spacing w:before="0" w:after="0"/></w:pPr><w:r><w:rPr><w:sz w:val="18"/><w:rFonts w:ascii="Arial" w:hAnsi="Arial"/></w:rPr><w:t>{nip_oleh}</w:t></w:r></w:p>
         </w:tc>
       </w:tr>
     </w:tbl>
+
+    <!-- KONFIGURASI HALAMAN A5 -->
+    <w:sectPr>
+      <w:pgSz w:w="8390" w:h="11907"/>
+      <w:pgMar w:top="432" w:right="576" w:bottom="432" w:left="576" w:header="288" w:footer="288"/>
+      <w:cols w:num="1"/>
+    </w:sectPr>
   </w:body>
 </w:document>`;
 };
@@ -165,13 +171,11 @@ export const exportDisposisiToDocx = (disposisi: DisposisiSurat) => {
       linebreaks: true,
     });
 
-    // Formatting Petugas: Nama dimasukkan seluruhnya pada nomor 1 (tidak berpindah ke nomor 2)
     const p1 = disposisi.petugas || '…………………………………………….';
     const p2 = '…………………………………………….';
     const p3 = '…………………………………………….';
     const p4 = '…………………………………………….';
 
-    // Checkbox Instruksi / Catatan
     const catatanList = disposisi.catatan || [];
     const hasCatatan = (keyword: string) =>
       catatanList.some((c) => c.toLowerCase().includes(keyword.toLowerCase()));
@@ -183,7 +187,6 @@ export const exportDisposisiToDocx = (disposisi: DisposisiSurat) => {
     const chkLain = disposisi.catatan_lain || hasCatatan('lain') ? '☑' : '☐';
     const catatanLainText = disposisi.catatan_lain ? disposisi.catatan_lain : '……………………………...';
 
-    // Format catatan: Setiap 1 kalimat / item dilanjutkan ke baris bawahnya (\n)
     let catatanFormatted = '';
     if (disposisi.catatan_text) {
       catatanFormatted = disposisi.catatan_text
@@ -195,7 +198,6 @@ export const exportDisposisiToDocx = (disposisi: DisposisiSurat) => {
         .join('\n');
     }
 
-    // Pastikan Nama Koordinator di bawah tanda tangan SELALU tercetak: Avianita Agustina, S.TP.
     let namaKoordinator = 'Avianita Agustina, S.TP.';
     if (disposisi.disposisi_oleh && disposisi.disposisi_oleh.trim()) {
       let cleaned = disposisi.disposisi_oleh
@@ -207,16 +209,16 @@ export const exportDisposisiToDocx = (disposisi: DisposisiSurat) => {
       }
     }
 
-    const dots = (len = 45) => '…'.repeat(len);
+    const dots = (len = 30) => '…'.repeat(len);
 
     doc.render({
-      surat_dari: disposisi.surat_dari || dots(45),
-      nomor_surat: disposisi.nomor_surat || dots(45),
-      tanggal_surat: disposisi.tanggal_surat || dots(45),
-      diterima_tanggal: disposisi.diterima_tanggal || dots(45),
-      nomor_agenda: disposisi.nomor_agenda || dots(45),
-      sifat: disposisi.sifat || dots(45),
-      hal: disposisi.hal || dots(45),
+      surat_dari: disposisi.surat_dari || dots(30),
+      nomor_surat: disposisi.nomor_surat || dots(30),
+      tanggal_surat: disposisi.tanggal_surat || dots(30),
+      diterima_tanggal: disposisi.diterima_tanggal || dots(30),
+      nomor_agenda: disposisi.nomor_agenda || dots(30),
+      sifat: disposisi.sifat || dots(30),
+      hal: disposisi.hal || dots(30),
       petugas_1: p1,
       petugas_2: p2,
       petugas_3: p3,
@@ -247,58 +249,58 @@ export const exportDisposisiToDocx = (disposisi: DisposisiSurat) => {
 };
 
 export const exportDisposisiToPdf = (disposisi: DisposisiSurat) => {
-  const pdf = new jsPDF("p", "mm", "a4");
+  const pdf = new jsPDF("p", "mm", "a5");
 
   pdf.setFont("helvetica", "bold");
-  pdf.setFontSize(16);
-  pdf.text("LEMBAR DISPOSISI", 105, 20, { align: "center" });
+  pdf.setFontSize(12);
+  pdf.text("LEMBAR DISPOSISI", 74, 12, { align: "center" });
 
   pdf.setFont("helvetica", "normal");
-  pdf.setFontSize(11);
+  pdf.setFontSize(9);
 
-  let y = 35;
+  let y = 20;
 
-  pdf.text(`Surat Dari : ${disposisi.surat_dari}`, 20, y); y += 8;
-  pdf.text(`No. Surat : ${disposisi.nomor_surat}`, 20, y); y += 8;
-  pdf.text(`Tanggal Surat : ${disposisi.tanggal_surat}`, 20, y); y += 8;
-  pdf.text(`Diterima : ${disposisi.diterima_tanggal}`, 20, y); y += 8;
-  pdf.text(`No. Agenda : ${disposisi.nomor_agenda}`, 20, y); y += 8;
-  pdf.text(`Sifat : ${disposisi.sifat}`, 20, y); y += 8;
-  pdf.text(`Hal : ${disposisi.hal}`, 20, y); y += 12;
+  pdf.text(`Surat Dari : ${disposisi.surat_dari}`, 12, y); y += 5;
+  pdf.text(`No. Surat : ${disposisi.nomor_surat}`, 12, y); y += 5;
+  pdf.text(`Tanggal Surat : ${disposisi.tanggal_surat}`, 12, y); y += 5;
+  pdf.text(`Diterima : ${disposisi.diterima_tanggal}`, 12, y); y += 5;
+  pdf.text(`No. Agenda : ${disposisi.nomor_agenda}`, 12, y); y += 5;
+  pdf.text(`Sifat : ${disposisi.sifat}`, 12, y); y += 5;
+  pdf.text(`Hal : ${disposisi.hal}`, 12, y); y += 7;
 
   pdf.setFont("helvetica", "bold");
-  pdf.text("Petugas:", 20, y);
+  pdf.text("Petugas:", 12, y);
   pdf.setFont("helvetica", "normal");
-  pdf.text(disposisi.petugas || "-", 45, y);
-  y += 10;
+  pdf.text(disposisi.petugas || "-", 32, y);
+  y += 6;
 
   pdf.setFont("helvetica", "bold");
-  pdf.text("Instruksi:", 20, y);
-  y += 7;
+  pdf.text("Instruksi:", 12, y);
+  y += 5;
 
   pdf.setFont("helvetica", "normal");
 
   if (disposisi.catatan?.length) {
     disposisi.catatan.forEach((c) => {
-      pdf.text("- " + c, 25, y);
-      y += 6;
+      pdf.text("- " + c, 16, y);
+      y += 4.5;
     });
   }
 
   if (disposisi.catatan_lain) {
-    pdf.text("- " + disposisi.catatan_lain, 25, y);
-    y += 6;
+    pdf.text("- " + disposisi.catatan_lain, 16, y);
+    y += 4.5;
   }
 
-  y += 15;
+  y += 8;
 
-  pdf.text("Koordinator", 145, y);
-  y += 25;
+  pdf.text("Koordinator", 95, y);
+  y += 14;
 
-  pdf.text(disposisi.disposisi_oleh || "Avianita Agustina, S.TP.", 145, y);
-  y += 6;
+  pdf.text(disposisi.disposisi_oleh || "Avianita Agustina, S.TP.", 95, y);
+  y += 4.5;
 
-  pdf.text(disposisi.nip_oleh || "NIP. 19720809 199903 2 007", 145, y);
+  pdf.text(disposisi.nip_oleh || "NIP. 19720809 199903 2 007", 95, y);
 
   pdf.save(`Disposisi_${disposisi.nomor_agenda}.pdf`);
 };
